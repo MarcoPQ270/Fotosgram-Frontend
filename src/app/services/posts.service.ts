ea28d63dc8c24d25e66b8744350b7839da9200c9
MarcoPQ270 <marco.balderaspq@gmail.com>
2020-02-27 13:34:45 -0600

    se agrego un servicio para traer la informacion del server y se empezo a editar el tab 1

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  paginaPost = 0;
+  constructor(private http: HttpClient) { }
+
+    getPosts() {
+       this.paginaPost++;
+       return this.http.get(`${URL}/posts/?pagina=${this.paginaPost}`);
+    }
+}
